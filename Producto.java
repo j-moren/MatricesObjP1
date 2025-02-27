1f0096375b434224482528277a8ce5d9691644a9
J-Moren <kalev.1503@gmail.com>
2025-02-27 02:04:33 -0500

    Se crea el Método Llenar Matriz

--- Producto.java
+++ Producto.java
@@ -3,4 +3,34 @@ public class Producto {
     private String Nombre;
     private double Precio;
     private int Cantidad;
+
+    //Contructor, Getter and Setters
+
+    public Producto() {
+
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.Nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.Precio = precio;
+    }
+
+    public int getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.Cantidad = cantidad;
+    }
 }
